1839a28e179930d7117c7232388e4df5c1f15864
seshansh <seshanshk123@gmail.com>
2023-04-07 20:21:28 +0530

    some chages

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,7 +1,18 @@
 import "./Contact.css"
+import Card from '../../components/Card'
+import { SiGmail } from "react-icons/si";
+import { AiFillLinkedin } from "react-icons/ai";
 const Contact = () => {
   return (
-    <section id='contact'><h2>Contact</h2></section>
+    <Card>
+      <section id='contact'><h2>Contact</h2>
+      {/* <div>
+      <a href="seshanshk123@gmail.com"> <SiGmail size="6rem" color="red"/> </a>
+      <a href=""><AiFillLinkedin /></a> */}
+      {/* </div> */}
+      
+      </section>
+    </Card>
   )
 }
 
