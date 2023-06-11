76a2b5b34546dfc32189b66e20d6272f0b61e8d3
seshansh kumar <seshanshk123@gmail.com>
2023-06-12 01:18:20 +0530

    css changes

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -3,17 +3,18 @@ import Card from '../../components/Card'
 import { SiGmail } from "react-icons/si";
 import { AiFillLinkedin } from "react-icons/ai";
 import { BiPhoneCall } from "react-icons/bi";
-import { BsGithub } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+
 
 const Contact = () => {
   return (
     <Card>
       <section id='contact'><h2>Contact</h2>
       <div className="contact-way">
-      <a className="contact-me" href="seshanshk123@gmail.com"> <SiGmail size="6rem" color="red"/> </a>
-      <a className="contact-me" href="https://www.linkedin.com/in/seshansh"><AiFillLinkedin size="6rem" color="blue"/></a>
-      <a className="contact-me" href="tel:8969138966"><BiPhoneCall size="6rem" color="green"/></a>
-      <a className="contact-me" href="https://github.com/seshansh?tab=repositories"><BsGithub size="6rem" color="black"/></a>
+      <a className="contact-me" href="mailto:seshanshk123@gmail.com"  target="_blank" rel="noreferrer"> <SiGmail size="6rem" color="red"/> </a>
+      <a className="contact-me" href="https://www.linkedin.com/in/seshansh" target="_blank" rel="noreferrer"><AiFillLinkedin size="6rem" color="blue"/></a>
+      <a className="contact-me" href="tel:8969138966" target="_blank" rel="noreferrer"><BiPhoneCall size="6rem" color="green"/></a>
+      <a className="contact-me" href="https://wa.me/918969138966?text=Hi Seshansh!" target="_blank" rel="noreferrer"><BsWhatsapp size="6rem" color="green"/></a>
 
       </div>
       
